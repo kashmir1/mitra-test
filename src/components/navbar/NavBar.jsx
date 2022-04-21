e2dff926b7eab7a78fcd39c22f1b7f6268954aca
kashmir1 <lzlin@mail.ru>
2022-04-21 15:54:59 +1000

    create menu toggler

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import './navbar.css'
 
 function NavBar() {
 
@@ -14,8 +15,18 @@ function NavBar() {
             <div className='container'>
                 <Navbar bg="light" expand="lg">
                     <Container>
-                        <Navbar.Brand href="#home">Галерея</Navbar.Brand>
-                        <button onClick={menuToggle}>Открыть меню</button>
+                        <div className="row">
+                            <div className='col-lg-6 position-relative'>
+                                <div className={!isMenuOpen ? 'toggle-open' : 'toggle-close'} onClick={menuToggle}></div>
+                            </div>
+
+                            <div className='col-lg-6'><Navbar.Brand href="#home">Галерея</Navbar.Brand></div>
+                        </div>
+                        <nav>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </nav>
                     </Container>
                 </Navbar>
             </div>
