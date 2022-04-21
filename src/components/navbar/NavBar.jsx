15fd09b061b6c9dd5f72b9eb7ecfcfb848220697
kashmir1 <lzlin@mail.ru>
2022-04-21 15:38:39 +1000

    create NavBar component

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './App.css';
-import {Button, Card, Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import React from "react";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
-function App() {
+function NavBar() {
     return (
-        <div className="App">
-            <div className="container">
+        <React.Fragment>
+            <div className='container'>
                 <Navbar bg="light" expand="lg">
                     <Container>
                         <Navbar.Brand href="#home">Галерея</Navbar.Brand>
@@ -26,20 +25,8 @@ function App() {
                     </Container>
                 </Navbar>
             </div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    );
+        </React.Fragment>
+    )
 }
 
-export default App;
-
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import App from './components/app/App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
