6269f000e4f3ce77ae8e57f8c7fa3ba11a85f24a
Alex <lzlin@mail.ru>
2022-04-21 22:08:14 +1000

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './components/app/App'
-import {createStore} from "redux";
+import { compose, createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/rootReducer";
 
-const store = createStore();
+const store = createStore(rootReducer, compose(
+    +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <React.Fragment>
-            <App />
-        </React.Fragment>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.Fragment>
+                <App/>
+            </React.Fragment>
+        </BrowserRouter>
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
