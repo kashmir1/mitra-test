d99bceb7b6f4e29e159120604ad91fd2ed1b2c53
kashmir1 <lzlin@mail.ru>
2022-04-21 15:58:03 +1000

    add active link

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import './navbar.css'
 
 function NavBar() {
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [ isMenuOpen, setIsMenuOpen ] = useState(false);
 
     const menuToggle = () => {
         setIsMenuOpen(!isMenuOpen)
@@ -17,14 +18,22 @@ function NavBar() {
                     <Container>
                         <div className="row">
                             <div className='col-lg-6 position-relative'>
-                                <div className={!isMenuOpen ? 'toggle-open' : 'toggle-close'} onClick={menuToggle}></div>
+                                <div className={ !isMenuOpen ? 'toggle-open' : 'toggle-close' }
+                                     onClick={ menuToggle }></div>
                             </div>
 
                             <div className='col-lg-6'><Navbar.Brand href="#home">Галерея</Navbar.Brand></div>
                         </div>
                         <nav>
-                            <ul>
-                                <li></li>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <NavLink className={ ({ isActive }) => (isActive ? 'active' : '') }
+                                             to="/">Галерея</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className={ ({ isActive }) => (isActive ? 'active' : '') } to="/about">Обо
+                                        мне</NavLink>
+                                </li>
                             </ul>
                         </nav>
                     </Container>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
 import App from './components/app/App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>
+    <BrowserRouter>
+        <React.Fragment>
+            <App />
+        </React.Fragment>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
